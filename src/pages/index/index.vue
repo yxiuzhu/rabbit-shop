16f06b5fd031fa61747bef0f84262f22df90abe7
y_xiuzhu <y_xiuzhu@163.com>
2023-08-15 23:52:11 +0800

    feat: 添加uniapp组件库，type类型配置

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,7 +3,12 @@
 </script>
 
 <template>
-  <view class="index">index</view>
+  <view class="index"
+    >index
+    <uni-card title="基础卡片" extra="额外信息">
+      <text>这是一个基础卡片示例，此示例展示了一个标题加标题额外信息的标准卡片。</text>
+    </uni-card>
+  </view>
 </template>
 
 <style lang="scss">
