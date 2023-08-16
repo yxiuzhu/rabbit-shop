2331ea6c65b3115acd76f0cc94764a295d3709b8
y_xiuzhu <y_xiuzhu@163.com>
2023-08-17 00:25:59 +0800

    feat: 【首页】顶部适应手机，新增轮播图组件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,14 +1,29 @@
 <script setup lang="ts">
-//
+import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home'
+import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
+
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+
+import CustomNavbar from './components/CustomNavbar.vue'
+
+// 获取轮播图数据
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+// 小程序特有的钩子函数：onLoad 页面加载
+onLoad(async () => {
+  await Promise.all([getHomeBannerData()])
+})
 </script>
 
 <template>
-  <view class="index"
-    >index
-    <uni-card title="基础卡片" extra="额外信息">
-      <text>这是一个基础卡片示例，此示例展示了一个标题加标题额外信息的标准卡片。</text>
-    </uni-card>
-  </view>
+  <!-- 自定义导航栏 -->
+  <CustomNavbar />
+  <!-- 自定义轮播图 -->
+  <XtxSwiper :list="bannerList" />
 </template>
 
 <style lang="scss">
