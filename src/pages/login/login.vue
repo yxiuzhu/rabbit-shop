dcb75bb26b071620a6c77ab516b7bee69843b0b6
y_xiuzhu <y_xiuzhu@163.com>
2023-08-16 09:27:24 +0800

    feat: 请求函数封装：成功、失败

---
+++ src/pages/login/login.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <view class="index">登录 </view>
+</template>
+
+<style lang="scss">
+//
+</style>
--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
-import '@/utils/http'
+import { http } from '@/utils/http'
 
 const memberStore = useMemberStore()
-const getData = () => {
-  uni.request({
+const getData = async () => {
+  // string[]是Data中result的T泛型的类型
+  const res = await http<string[]>({
     method: 'GET',
-    url: '/home/banner',
+    url: '/member/profile',
   })
+  console.log('请求成功', res)
 }
 </script>
 
